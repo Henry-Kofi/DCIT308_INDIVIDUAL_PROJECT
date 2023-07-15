16830d67941c0de626daa01bc6ac210f37d78ece
Henry-Kofi <hnyonyo001@st.ug.edu.gh>
2023-07-15 13:34:27 +0000

    added dependencies for mysql connections

--- nhenry/src/main/java/nyonyo589/finalproject/WelcomeController.java
+++ nhenry/src/main/java/nyonyo589/finalproject/WelcomeController.java
@@ -63,6 +63,11 @@ public class WelcomeController implements Initializable {
             Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
             stackPaneView.getChildren().removeAll();
             stackPaneView.getChildren().setAll(root);
+
+            dashboardButton.setStyle("-fx-background-color:  #4c214b;\n" +
+                    "-fx-border:solid;\n" +
+                    "-fx-border-width:0px 0px 0px 3px;\n" +
+                    "-fx-border-color: #9a1d5f;");
         } catch (IOException e) {
 //            throw new RuntimeException(e);
             Logger.getLogger(WelcomeController.class.getName()).log(Level.SEVERE,null,e);
@@ -72,20 +77,75 @@ public class WelcomeController implements Initializable {
         Parent root = FXMLLoader.load(getClass().getResource("add-goods.fxml"));
         stackPaneView.getChildren().removeAll();
         stackPaneView.getChildren().setAll(root);
+        viewaddgoodsButton.setStyle("-fx-background-color:  #4c214b;\n" +
+                "-fx-border:solid;\n" +
+                "-fx-border-width:0px 0px 0px 3px;\n" +
+                "-fx-border-color: #9a1d5f;");
+        dashboardButton.setStyle("-fx-background-color: #140620;\n" +
+                "-fx-background-radius:0px 5px 5px 0px;\n" +
+                "-fx-border-radius:0px;");
+        viewvendorsButton.setStyle("-fx-background-color: #140620;\n" +
+                "-fx-background-radius:0px 5px 5px 0px;\n" +
+                "-fx-border-radius:0px;");
+        viewissuegoodsButton.setStyle("-fx-background-color: #140620;\n" +
+                "-fx-background-radius:0px 5px 5px 0px;\n" +
+                "-fx-border-radius:0px;");
     }
     public void dashboardButtonOnAction(ActionEvent event) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
         stackPaneView.getChildren().removeAll();
         stackPaneView.getChildren().setAll(root);
+
+        dashboardButton.setStyle("-fx-background-color:  #4c214b;\n" +
+                "-fx-border:solid;\n" +
+                "-fx-border-width:0px 0px 0px 3px;\n" +
+                "-fx-border-color: #9a1d5f;");
+        viewaddgoodsButton.setStyle("-fx-background-color: #140620;\n" +
+                "-fx-background-radius:0px 5px 5px 0px;\n" +
+                "-fx-border-radius:0px;");
+        viewvendorsButton.setStyle("-fx-background-color: #140620;\n" +
+                "-fx-background-radius:0px 5px 5px 0px;\n" +
+                "-fx-border-radius:0px;");
+        viewissuegoodsButton.setStyle("-fx-background-color: #140620;\n" +
+                "-fx-background-radius:0px 5px 5px 0px;\n" +
+                "-fx-border-radius:0px;");
     }
     public void viewvendorsButtonOnAction(ActionEvent event) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("vendors-view.fxml"));
         stackPaneView.getChildren().removeAll();
         stackPaneView.getChildren().setAll(root);
+
+        viewvendorsButton.setStyle("-fx-background-color:  #4c214b;\n" +
+                "-fx-border:solid;\n" +
+                "-fx-border-width:0px 0px 0px 3px;\n" +
+                "-fx-border-color: #9a1d5f;");
+        dashboardButton.setStyle("-fx-background-color: #140620;\n" +
+                "-fx-background-radius:0px 5px 5px 0px;\n" +
+                "-fx-border-radius:0px;");
+        viewaddgoodsButton.setStyle("-fx-background-color: #140620;\n" +
+                "-fx-background-radius:0px 5px 5px 0px;\n" +
+                "-fx-border-radius:0px;");
+        viewissuegoodsButton.setStyle("-fx-background-color: #140620;\n" +
+                "-fx-background-radius:0px 5px 5px 0px;\n" +
+                "-fx-border-radius:0px;");
     }
     public void viewissuegoodsButtonOnAction(ActionEvent event) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("issue-goods.fxml"));
         stackPaneView.getChildren().removeAll();
         stackPaneView.getChildren().setAll(root);
+
+        viewissuegoodsButton.setStyle("-fx-background-color:  #4c214b;\n" +
+                "-fx-border:solid;\n" +
+                "-fx-border-width:0px 0px 0px 3px;\n" +
+                "-fx-border-color: #9a1d5f;");
+        viewvendorsButton.setStyle("-fx-background-color: #140620;\n" +
+                "-fx-background-radius:0px 5px 5px 0px;\n" +
+                "-fx-border-radius:0px;");
+        dashboardButton.setStyle("-fx-background-color: #140620;\n" +
+                "-fx-background-radius:0px 5px 5px 0px;\n" +
+                "-fx-border-radius:0px;");
+        viewaddgoodsButton.setStyle("-fx-background-color: #140620;\n" +
+                "-fx-background-radius:0px 5px 5px 0px;\n" +
+                "-fx-border-radius:0px;");
     }
 }
--- nhenry/src/main/java/nyonyo589/finalproject/vendorsViewController.java
+++ nhenry/src/main/java/nyonyo589/finalproject/vendorsViewController.java
@@ -1,4 +1,18 @@
 package nyonyo589.finalproject;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
 public class vendorsViewController {
+    @FXML
+    private ChoiceBox<String> categoryChoiceBox;
+
+    String[] categoryChoices = {"","Beverages","Bread/Bakery","Canned/Jarred Goods","Dairy Products"
+            ,"Dry/Baking Goods","Frozen Products","Meat","Farm Produce","Home Cleaners",
+            "Paper Goods","Home Care"};
+
+    @FXML
+    private void initialize(){
+        categoryChoiceBox.getItems().addAll(categoryChoices);
+    }
 }
